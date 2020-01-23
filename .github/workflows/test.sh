facafe42b8a6e06f0b0965bb9a6733417d8b113d
Vladimir Orany <vladimir@orany.cz>
2020-01-23 09:48:04 +0100

    testing micronaut groovy function generator

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -9,6 +9,7 @@ sdk install lazybones
 
 cd /tmp
 
+# test kordamp-groovy-test template
 lazybones create kordamp-groovy 1.1.0-SNAPSHOT kordamp-groovy-test \
  -Pid=kordamp-groovy-test \
  -Porg=agorapulse \
@@ -22,3 +23,30 @@ lazybones create kordamp-groovy 1.1.0-SNAPSHOT kordamp-groovy-test \
  -Pbintray.org=agorapulse\
  -Pbintray.repo=libs\
 
+cd kordamp-groovy-test
+./gradlew test
+
+# test kordamp-groovy-test template
+test_mfg req, resp
+test_mfg map, string
+test_mfg ap, ap
+
+function test_mfg() {
+    lazybones create micronaut-function-groovy 1.1.0-SNAPSHOT mfg-test \
+      -Pname=MfgTest \
+      -Pstandalone=yes \
+      -Pslug=agorapulse/mfg-test \
+      -Pgroup=com.agorapulse \
+      -Ppkg=com.agorapulse.mfg.test \
+      -Pregion=eu-west-1 \
+      -Pprofile=beta \
+      -Pport=8080 \
+      -Pinput=$1 \
+      -Poutput=$2 \
+      -Plibs=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
+
+  cd mfg-test
+  ./gradlew test
+
+  cd ..
+}
