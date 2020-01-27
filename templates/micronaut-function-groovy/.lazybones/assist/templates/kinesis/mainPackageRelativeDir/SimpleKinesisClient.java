84137314a8501c89cea3c2ee05d74daca7cee012
Vladimir Orany <vladimir@orany.cz>
2020-01-27 07:53:53 +0100

    more example files

---
+++ templates/micronaut-function-groovy/.lazybones/assist/templates/kinesis/mainPackageRelativeDir/SimpleKinesisClient.java
@@ -0,0 +1,17 @@
+package $pkg;
+
+import com.agorapulse.micronaut.aws.kinesis.annotation.KinesisClient;
+import com.amazonaws.services.kinesis.model.PutRecordResult;
+
+@KinesisClient
+public interface SimpleKinesisClient {
+
+    /**
+     * Puts a record into Kinesis stream.
+     *
+     * @param record record to be pushed into the stream, you can change this to object which will be serialized automatically
+     * @return result of the operation
+     * @link https://agorapulse.github.io/micronaut-libraries/#_publishing_with_kinesisclient
+     */
+    PutRecordResult putRecordString(String record);
+}
