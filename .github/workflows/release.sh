4bfc3f0b15d78e9e4a3d83c6f99f70ff2541715a
Vladimir Orany <vladimir@orany.cz>
2021-05-13 17:39:13 +0200

    fixed release script

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -7,15 +7,22 @@ do
     echo $RELEASE_VERSION > $file
 done
 
+export LAZYBONES_REPO_DIR=/tmp/lazybones
+git clone https://github.com/agorapulse/lazybones "$LAZYBONES_REPO_DIR"
+
+$LAZYBONES_REPO_DIR/gradlew -p "$LAZYBONES_REPO_DIR" install installDist -x test -x integTest
+
+export PATH="$PATH:$LAZYBONES_REPO_DIR/lazybones-app/build/install/lazybones/bin"
+
 ./gradlew publishAllTemplates
 
-uploadFile "build/packages/kordamp-groovy-template-$RELEASE_VERSION.zip"
-uploadFile "build/packages/micronaut-function-groovy-template-$RELEASE_VERSION.zip"
+uploadFile "Kordamp Groovy Template" "build/packages/kordamp-groovy-template-$RELEASE_VERSION.zip"
+uploadFile "Micronaut Function Groovy Template" "build/packages/micronaut-function-groovy-template-$RELEASE_VERSION.zip"
 
 function uploadFile() {
     curl \
     -H "Authorization: token $GITHUB_TOKEN" \
-    -H "Content-Type: $(file -b --mime-type $1)" \
-    --data-binary @$1 \
-    "$UPLOAD_URL?name=$(basename $1)"
+    -H "Content-Type: $(file -b --mime-type $2)" \
+    --data-binary @$2 \
+    "$UPLOAD_URL?name=$(basename $2)&label=$1"
 }
